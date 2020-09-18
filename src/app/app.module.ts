5df2b3ce5717ced6925b369256cb30e57995fd96
Alexis DRAI <drai.alexis@gmail.com>
2020-09-18 19:29:45 +0200

    Prepare basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FooterComponent } from './components/page-structure/footer/footer.component';
+import { HeaderComponent } from './components/page-structure/header/header.component';
+import { DescriptionComponent } from './components/description/description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule,
