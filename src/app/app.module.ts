629f3e114284cdea0e9b4aa0ead5f490639d8a35
Alexis DRAI <drai.alexis@gmail.com>
2020-09-18 20:11:00 +0200

    Add jQuery and Bootstrap dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FooterComponent } from './components/page-structure/footer/footer.component';
-import { HeaderComponent } from './components/page-structure/header/header.component';
-import { DescriptionComponent } from './components/description/description.component';
+import {AppComponent} from './app.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {FooterComponent} from './components/page-structure/footer/footer.component';
+import {HeaderComponent} from './components/page-structure/header/header.component';
+import {DescriptionComponent} from './components/description/description.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,9 +17,11 @@ import { DescriptionComponent } from './components/description/description.compo
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
