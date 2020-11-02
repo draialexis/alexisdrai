2b8418ae101a23303bfaccb9a2439cdff0f8f864
Alexis DRAI <drai.alexis@gmail.com>
2020-11-02 03:00:56 +0100

    Break everything trying to use routers

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MainComponent} from './components/main/main.component';
+import {SmokefreeComponent} from './components/smokefree/smokefree.component';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  {path: 'main', component: MainComponent},
+  {path: 'smokefree', component: SmokefreeComponent},
+  {path: '', redirectTo: '/main', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+
 import {FooterComponent} from './components/page-structure/footer/footer.component';
 import {HeaderComponent} from './components/page-structure/header/header.component';
+
 import {MainComponent} from './components/main/main.component';
 import {SmokefreeComponent} from './components/smokefree/smokefree.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    NgbModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
     MainComponent,
-    SmokefreeComponent
-  ],
-  imports: [
-    BrowserModule,
-    FontAwesomeModule,
-    NgbModule
+    SmokefreeComponent,
+    PageNotFoundComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
