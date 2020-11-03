491d624b1958ed1c4c29c20fe951cbd1262e1e4e
Alexis DRAI <drai.alexis@gmail.com>
2020-11-03 23:33:16 +0100

    Comment out router + Add entries 1 and 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {AppRoutingModule} from './app-routing.module';
+// import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
@@ -29,7 +29,7 @@ import {FormsModule} from '@angular/forms';
     FontAwesomeModule,
     NgbModule,
     FormsModule,
-    AppRoutingModule
+    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
